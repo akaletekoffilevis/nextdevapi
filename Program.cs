a6774c206aa0d01fdffb40b1d792e61dd9e816ab
akaletekoffilevis <l.akalete20@gmail.com>
2025-12-27 01:21:55 +0100

    Configure Swagger for DANKASSAWA NEXTDEV API
    Added Swagger documentation configuration with API details.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
 using NextDevAsp.Api;
 using NextDevAsp.Api.DataContext;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,10 +20,36 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt => 
+{
+   opt.SwaggerDoc("v1", new OpenApiInfo{
+        Title = "DANKASSAWA NEXTDEV API",
+        Version = "v1",
+        Description = "CRUD API FOR NEXTDEV TEAMS",
+        Contact = new OpenApiContact
+        {
+              Name = "DANKASSAWA TEAM",
+              Email = "l.akalete20@gmail.com" 
+        }
+   });
+});
+
+
+//builder.Services.AddControllers();
 
 var app = builder.Build();
 app.UseCors("CorsPolicy");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.DocumentTitle = "DANKASSAWA NextDEv API";
+        //options.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+        options.RoutePrefix = "swagger";
+    });
+};
+
 app.UseForwardedHeaders(new ForwardedHeadersOptions
 {
     ForwardedHeaders = ForwardedHeaders.All
